97e94b9c6a6ea4d7f4d3a4ead87213bcaa1b3bff
Rohith Pentaparthy <penta@umich.edu>
2017-08-25 23:38:27 -0400

    fixed URI

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import urllib.parse as ul
 
 
 ul.uses_netloc.append("postgres")
-url = ul.urlparse(os.environ["postgres://itlnykuwjunoll:a5bd6c66146478a7b4b202a0e59f0789536a746093495e22c1ba0bebca8af3ac@ec2-23-23-225-12.compute-1.amazonaws.com:5432/dbli9ucgjn3"])
+url = ul.urlparse(os.environ["postgres://itlnykuwjunoll:a5bd6c66146478a7b4b202a0e59f0789536a746093495e22c1ba0bebca8af3ac@ec2-23-23-225-12.compute-1.amazonaws.com:5432/dbli9ucgjn3501"])
 
 conn = psycopg2.connect(
     database=url.path[1:],
